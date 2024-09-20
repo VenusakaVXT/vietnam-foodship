1a32ca29e81c3f6cc3142b6fc1014c36868a9086
VenusakaVXT <voxuantuan33@gmail.com>
2024-09-20 21:50:04 +0700

    Add Logo && convert tailwind.config from .js to .ts (#7)

---
+++ client/src/components/Logo/Logo.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import "../../scss/Logo.scss"
+
+interface LogoProps {
+    width: number
+    height: number
+    fontSize: string
+    bgColor: string
+    color: string
+    lineDown: boolean
+}
+
+const Logo: React.FC<LogoProps> = ({ width, height, fontSize, bgColor, color, lineDown }) => {
+    return (
+        <>
+            <div style={{
+                width,
+                height,
+                background: bgColor,
+                borderRadius: "20px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <h1 className="app-name" style={{ fontSize, color }}>
+                    {lineDown ? "VietNam FoodShip" : <>VietNam<br />FoodShip</>}
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default Logo
